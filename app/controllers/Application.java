799bafa4f75517f785db9be04b2b53824902c042
miyohide <miyohide@gmail.com>
2014-09-24 21:54:54 +0900

    connect db.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,29 @@
 package controllers;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
+import play.db.*;
 
 public class Application extends Controller {
 
     public static Result index() {
+        Connection connection = DB.getConnection();
+        try {
+            PreparedStatement sql = connection
+                    .prepareStatement("SELECT * FROM member");
+            ResultSet rs = sql.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return ok(index.render("Your new application is ready."));
     }
 
