b6db1876af60a72af7704ebcfb0e27c528836050
miyohide <miyohide@gmail.com>
2014-09-28 23:15:17 +0900

    print Member list.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
+import models.Member;
 import play.*;
 import play.mvc.*;
 import views.html.*;
@@ -13,18 +15,8 @@ import play.db.*;
 public class Application extends Controller {
 
     public static Result index() {
-        Connection connection = DB.getConnection();
-        try {
-            PreparedStatement sql = connection
-                    .prepareStatement("SELECT * FROM member");
-            ResultSet rs = sql.executeQuery();
-            while (rs.next()) {
-                System.out.println(rs.getString("name"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return ok(index.render("Your new application is ready."));
+        List<Member> members = Member.find.all();
+        return ok(index.render(members));
     }
 
 }
